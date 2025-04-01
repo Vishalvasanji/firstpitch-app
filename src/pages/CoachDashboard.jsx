dd9c5bb45bd9bc3eaf3564189e4cdc5078ac24aa
Vishalvasanji <49619311+Vishalvasanji@users.noreply.github.com>
2025-04-01 15:21:43 -0500

    Create CoachDashboard.jsx

---
+++ src/pages/CoachDashboard.jsx
@@ -0,0 +1,7 @@
+export default function CoachDashboard() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <h1 className="text-2xl font-semibold text-gray-800">Welcome to your dashboard, Coach!</h1>
+    </div>
+  );
+}
