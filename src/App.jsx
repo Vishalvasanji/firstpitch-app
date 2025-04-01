91a3847fce1a503dd163644a0a92844f8d95de13
Vishalvasanji <49619311+Vishalvasanji@users.noreply.github.com>
2025-04-01 15:40:22 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CoachAuthPage from "./pages/CoachAuthPage";
-import CoachDashboard from "./pages/CoachDashboard"; // placeholder for now
+import CreateTeam from "./pages/CreateTeam";
+import CoachDashboard from "./pages/CoachDashboard";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
@@ -8,6 +9,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<CoachAuthPage />} />
+        <Route
+          path="/create-team"
+          element={
+            <PrivateRoute>
+              <CreateTeam />
+            </PrivateRoute>
+          }
+        />
         <Route
           path="/dashboard"
           element={
@@ -22,4 +31,3 @@ function App() {
 }
 
 export default App;
-
